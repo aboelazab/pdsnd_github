1ff77d9096792b3403c94aca812c1ec3c9894ac9
Saad Aboelazab <s.aboelazab@gmail.com>
2019-10-28 08:01:11 +0200

    Modifying Doc strings
    Adding a hint in user_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -154,7 +154,11 @@ def trip_duration_stats(df):
     print('-'*100)
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """
+    Displays statistics on bikeshare users.
+    HINT:
+         user stats are not applicable in Washington City data
+    """
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
